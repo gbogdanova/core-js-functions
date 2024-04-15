a4cfa986863521bcf3cb59a865d84f89b49e2467
Galya Bogdanova <galya_bogdanova@MacBook-Air-Maksim.local>
2024-04-15 14:20:30 -0700

    feat: add getIdGeneratorFunction function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -235,8 +235,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const insertId = id;
+    id += 1;
+    return insertId;
+  };
 }
 
 module.exports = {
